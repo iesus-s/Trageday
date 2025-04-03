50366719573a6528498de397b0fbc99c06f4d1c6
iesus-s <sandoval.j4@outlook.com>
2025-04-03 10:28:05 -0700

    update google ads

--- site/pages/currentevents.php
+++ site/pages/currentevents.php
@@ -62,6 +62,7 @@ echo "<!DOCTYPE html>
     <link rel='stylesheet' type='text/css' href='../styles/style.css?v=1'> 
     <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css' rel='stylesheet' 
     integrity='sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH' crossorigin='anonymous'>
+    <meta name='google-adsense-account' content='ca-pub-9004547571569120'>
     <title>Trageday</title>  
 </head>";
 
